99c3733a269592e5c1c98c4b2f7f272c134ff5ff
Nurmurod <elow9020@gmail.com>
2023-10-06 00:14:20 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,252 @@
+// begin_1
+
+// const a = parseInt(prompt("Kvadiratning tomonini kiriting"))
+
+// const P = 4 * a
+
+// alert(P)
+
+
+
+// begin_2
+
+// const a = parseInt(prompt("Kvadiratning yuzini kirting"))
+
+// const S = a * a
+
+// alert(S)
+
+
+
+// begin_3
+
+// const a = parseInt(prompt("To'g'ri to'rtburchakning bo'yini kiriting"))
+// const b = parseInt(prompt("To'g'ri to'rtburchakning enini kirting"))
+
+// let S = a * b
+// let P = 2*(a+b)
+
+// alert(`To'gri to'rtburchakning Yuzasi hisoblanadi ${S}`)
+// alert(`To'gri to'rtburchakning Peremetri hisoblanadi ${P}`)
+
+
+
+
+//begin_4
+
+
+// var d = parseInt(prompt("Aylana shaklining diametrini kiriting"))
+
+
+// let a = 3.14
+// let L = a*d
+
+
+// alert(`Aylana shaklinig diametri hisoblanadi ${L}`)
+
+
+
+
+
+//begin_5
+
+// const a = parseInt(prompt("Kubning tomonlarini kiriting"))
+
+
+// let V = a*a*a
+// var S = 6*a*a
+
+
+// alert(`Kubning hajmi hisoblanadi ${V}`)
+// alert(`Kubning to'la sirti hisoblanadi ${S}`)
+
+
+
+
+//begin_6
+// let a = parseInt(prompt("Paralelepipedning tomonlarini kirting"))
+// let b = parseInt(prompt("Paralelepipedning tomonlarini kirting"))
+// let c = parseInt(prompt("Paralelepipedning tomonlarini kirting"))
+
+
+// const V = a*b*c
+// const S = 2*(a * b + b *c + a * c )
+
+
+// alert(`Paralelepipedning hajmi hisoblanadi ${V}`)
+// alert(`Paralelepipedning to'la sirti hisoblanadi ${S}`)
+
+
+
+
+//begin_7
+
+// const R = parseInt(prompt("Doiraning radiusini kirting"))
+
+
+
+// let L = 2*a*R
+// var S = a*R*R
+
+
+// alert(`Doiraning uzunligi hisoblanadi ${L}`)
+// alert(`Doiraning yuzasi hisoblanadi ${S}`)
+
+
+
+
+//begin_8
+
+// var a = parseInt(prompt("a uchun ega xoxlagan son kiriting"))
+// var b = parseInt(prompt("b uchun ega xoxlagan son kiriting"))
+
+
+// let L = (a+b)/2
+
+
+// alert(`O'rta arifmetigi hisoblanadi ${L}`)
+
+
+
+
+
+//begin_13
+
+// var r1 = parseInt(prompt("berilgan aylananing radiusini kiriting"));
+// var r2 = parseInt(prompt("berilgan aylananing radiusini kiriting"));
+
+
+// var s1 = 2*r1
+// var s2 = 2*r2
+// var s3 = 2*(r1-r2)
+
+
+// alert (`Yuza ${s1}`)
+// alert (`Yuza ${s2}`)
+// alert (`ayirmasi ${s3}`)
+
+
+
+//begin32
+
+// var f = parseInt(prompt("Xoxlagan gradus kiriting"))
+
+// let S = (f-32)*5/9
+
+// alert(`Temperatura Farengeytenga teng ${S}`)
+
+
+
+
+
+
+                        //integer
+
+//integer_1
+
+// var L = parseInt(prompt("Xoxlagan santimetr kiriting"))
+
+// var a = Math.floor(L/100)
+
+// alert(`shuncha metr: ${a}`)
+
+
+
+
+
+
+//integer_2
+
+// var L = parseInt(prompt("Xoxlagan kilogramm kiriting"))
+
+// var a = Math.floor(L/1000)
+
+// alert(`shuncha tonna: ${a}`)
+
+
+
+
+//integer_3
+
+//  var L = parseInt(prompt("Xoxlagan bayt kiriting"))
+
+//  var a = Math.floor(L/1024)
+
+//  alert(`shuncha kb: ${a}`)
+
+
+
+
+
+//integer_4
+
+// var A = parseInt(prompt("Nechta orindiq borligini yozing"))
+// var B = parseInt(prompt("Odam sonini yozing"))
+
+// var S = Math.floor(A-B)
+
+// alert(`Shuncha joylashtirish mumkin: ${S}`)
+
+
+
+
+//integer_19
+
+//  var n = parseInt(prompt("Xoxlagan bir son kiriting"))
+
+//  let L = Math.floor(n/60)
+
+//  alert (`Kun boshidan boshlab shuncha daqiqa o'tdi ${L}`)
+
+
+
+
+//integer_20
+
+//  var n = parseInt(prompt("Xoxlagan bir son kiriting"))
+
+//  let L = Math.floor(n/3600)
+
+//  alert (`Kun boshidan boshlab shuncha soat o'tdi ${L}`)
+
+
+
+
+
+//integer_21
+
+// var n = parseInt(prompt("Xoxlagan bir son kiriting"))
+
+// let L = Math.floor(n/60)
+// let M = Math.floor(n%60)
+
+// alert (`Kun boshidan boshlab shuncha ${M} daqiqa, va ${L} sekund otdi`)
+
+
+
+
+
+
+//integer_22
+
+// var n = parseInt(prompt("Xoxlagan bir son kiriting"))
+
+// let L = Math.floor(n/60)
+// let M = Math.floor(n%3600)
+
+// alert (`Kun boshidan boshlab shuncha ${M} soat, va ${L} sekund otdi`)
+
+
+
+
+
+
+
+//integer_23
+
+// var n = parseInt(prompt("Xoxlagan bir son kiriting"))
+
+// let L = Math.floor(n%60)
+// let M = Math.floor(n%3600)
+
+// alert (`Kun boshidan boshlab shuncha ${M} soat, va ${L} daqiqa otdi`)
